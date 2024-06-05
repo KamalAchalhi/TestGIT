2be28ea28ca01d3697d459d8c6699905f2a18f1e
Kamal ACHALHI <achalhi.k@intlocal.univ-amu.fr>
2024-06-05 15:09:13 +0200

    Message pour commenté pour moi même

---
+++ Kamal.py
@@ -0,0 +1,7 @@
+x=3 #il faut une valeur différente de 0 pour rentrer dans la boucle
+while(x!=0):
+x=int(input("Rentrer la valeur de x : "))
+resultat=x+2
+resultat=resultat*2
+print("Quand x vaut "+repr(x)+", le résultat vaut "+repr(resultat)+" !\n")
+print("Happy end for everybody!")
